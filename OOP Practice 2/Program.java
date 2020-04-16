554bb6946be8ae94f155dd073e06c9dd927b8327
ornaverum <54825956+ornaverum@users.noreply.github.com>
2020-04-15 21:16:39 -0500

    update grass

--- OOP Practice 2/Program.java
+++ OOP Practice 2/Program.java
@@ -10,25 +10,23 @@ public class Program
 {
 	public static void main(String[] args)
     {
-		ecosystem eco = new ecosystem(20, 20, 0.1, 0.05);
+		ecosystem eco = new ecosystem(10, 10, 0.1, 0);
 	}
-	
-	
 }
 
 interface being{
-	being tryToReproduce();
-	
+	being reproduce();
+
 	int[] getPos();
-	
+
 	void setPos(int x, int y);
-	
+
 	void feed(int dE);
-	
+
 	int getEnergy();
-	
+
 	void setEnergy(int E);
-	
+
 	int die();
 }
 
@@ -36,92 +34,105 @@ interface being{
 class grass implements being{
 	int x, y;
 	int energy;
-	public static final int REPRODUCTION_ENERGY_THRESHOLD = 2;
-	
+	public static final int REPRODUCTION_ENERGY_COST = 2;
+
 	grass(){
 		x = 0;
 		y = 0;
 		energy = 0;
 	}
-	
+
 	grass(int x, int y, int e){
 		this.x = x;
 		this.y = y;
 		energy = e;
 	}
-	
+
 	public void setPos(int x, int y){
 		this.x = x;
 		this.y = y;
 	}
-	
-	public int[] getPos(){ return {x, y}; }
-	
-	public int getEnergy(){	return energy;}
-	
-	public void setEnergy(int e) { energy = e; }
-	
-	public void tryToReproduce(){
-		Random rand = new Random();
-		int dx, dy;
-		dx = rand.nextInt(-1,1);
-		dy = rand.nextInt(-1,1);
-		
-		
+
+	public int[] getPos(){
+		int[] pos = {x, y};
+		return pos;
 	}
-	
+
+	public int getEnergy(){	return energy;}
+
+	public void setEnergy(int e) { energy = e; }
+
+	public String toString(){
+		String s = String.format("Type: %s at pos = (%d, %d)", this.getClass(), x, y);
+		return s;
+	}
+
+	public int die(){return 0;}
+
+	public void feed(int dE){energy += dE;}
+
+	public grass reproduce(){
+		if (energy > REPRODUCTION_ENERGY_COST){
+			energy -= REPRODUCTION_ENERGY_COST;
+			grass g = new grass();
+			return g;
+		} else { return null; }
+	}
 }
 
 class critter implements being{
 	int x, y;
 	int energy;
-	
-	grass(){
+
+	critter(){
 		x = 0;
 		y = 0;
 		energy = 0;
 	}
-	
-	grass(int x, int y, int e){
+
+	critter(int x, int y, int e){
 		this.x = x;
 		this.y = y;
 		energy = e;
 	}
-	
+
 	public void setPos(int x, int y){
 		this.x = x;
 		this.y = y;
 	}
-	
-	public int[] getPos(){ return {x, y}; }
-	
+
+	public int[] getPos(){
+		int[] pos = {x, y};
+		return pos;
+	}
 	public int getEnergy(){	return energy;}
-	
+
 	public void setEnergy(int e) { energy = e; }
-	
+
+	public critter reproduce(){}
+
+	public int die(){return 0;}
+
+	public void feed(int dE){energy += dE;}
+
 	public String toString(){
 		String s = String.format("Type: %s at pos = (%d, %d)", this.getClass(), x, y);
 		return s;
 	}
-	
 }
 
-//~ class herbovore extends critter{
-	
-	
-}
 
 class ecosystem{
 	public int maxX, maxY;
 	public double grassDensity, herbDensity;
-	
+
 	public String[][] displayGrid;
-	
-	ArrayList<food> masterList = new ArrayList<>();
+
+	ArrayList<being> masterList = new ArrayList<>();
 
 	ecosystem(){
 	}
-	
+
 	ecosystem(int maxX, int maxY, double rhoG, double rhoH){
 		this.maxX = maxX;
 		this.maxY = maxY;
@@ -132,40 +143,37 @@ class ecosystem{
 		updateGrid();
 		drawGrid();
 	}
-	
+
 	public String toString(){
 		for (int i = 0; i < maxX; i ++){
 			for (int j = 0; j < maxY; j ++){
-				
+
 			}
 		}
 		return "";
 	}
-	
+
 	public void populate(){
-		Random rand = new Random(); 
+		Random rand = new Random();
 		double r;
 		for (int i = 0; i < maxX; i ++){
 			for (int j = 0; j < maxY; j ++){
 				r = rand.nextDouble();
 				if (r < grassDensity){
-					grass g = new grass(i, j);
+					grass g = new grass(i, j, 1);
 					masterList.add(g);
-				} else if ( r < grassDensity + herbDensity ){
-					herbovore h = new herbovore(i, j);
-					masterList.add(h);
 				}
 			}
 		}
 	}
-	
+
 	public void updateGrid(){
 		for (int i = 0; i < maxX; i ++){
 			for (int j = 0; j < maxY; j ++){
 				displayGrid[i][j] = ".";
 			}
 		}
-		for (food f:masterList){
+		for (being f:masterList){
 			int[] pos = f.getPos();
 			int i = pos[0];
 			int j = pos[1];
@@ -177,7 +185,7 @@ class ecosystem{
 			}
 		}
 	}
-	
+
 	public void drawGrid(){
 		for (int i = 0; i < maxX; i ++){
 			for (int j = 0; j < maxY; j ++){
@@ -186,161 +194,7 @@ class ecosystem{
 			System.out.printf("\n");
 		}
 	}
-	
+
 	public void updateCycle(){
-		ArrayList<food> nextList = (ArrayList<food>)masterList.clone();
-		for(food f:masterList){
-			if (f.foodType !="grass"){
-				((critter) f).see();
-			}
-		}
-		for(food f:masterList){
-			if (f.foodType !="grass"){
-				((critter) f).chasePrey(1);
-			}
-		}
-		
-		
-	}
-	
-}
-	
-	
-
-class critter extends food implements eater{
-	int energy = 10;
-	int speed = 5;
-	food prey = null;
-	String preyType = "";
-	
-	
-	critter(){
-		super();
-	}
-	
-	critter(int x, int y){
-		super(x, y);
-		this.x = x;
-		this.y = y;
-			
-	}
-	
-	critter(int x, int y, int Eg, int E){
-		super(x, y, Eg);
-		this.x = x;
-		this.y = y;
-		energyGained = Eg;
-		energy = E;
-	}
-	
-	public void move(int vx, int vy){
-		double eC = energyCost(vx, vy);
-		//~ System.out.printf("Entering move method.\n");
-		x += vx;
-		y += vy;
-		energy -= eC;
-	}
-	
-	public double energyCost(int vx, int vy){
-		return Math.pow(Math.pow(vx,2) + Math.pow(vy,2), 0.8);
-	}
-	
-	
-	public void eatPrey()
-	{
-		
-	}
-	
-	public void chasePrey(double dt)
-	{
-		if (prey != null){
-			int[] pos = prey.getPos();
-			
-			int xp = pos[0];
-			int yp = pos[1];
-			
-			int signX0 = Math.signum(x-xp);
-			int signY0 = Math.signum(x-yp);
-			
-			double theta = Math.atan2( (double)(yp-y), (double) (xp-x));
-			int vx = (int) Math.round(Math.cos(theta)*speed*dt);
-			int vy = (int) Math.round(Math.sin(theta)*speed*dt);
-			
-			move(vx, vy);
-			
-			int signX1 = Math.signum(x-xp);
-			int signY1 = Math.signum(y-yp);
-			
-			boolean b1 = signX0!=signX1 || signX0 == 0;
-			boolean b2 = signY0!=signY1 || signY0 == 0;
-			
-			if ( (b1 && b2) || (signX1 == 0 && signY1 == 0) ){
-				eatPrey();
-			}
-			
-		}
-	}
-	
-	public void see(double r, ArrayList<food> FoodList){
-		double closest = 1000000.0;
-		double distance = 0;
-		String foodName = "";
-		
-		for (food F: FoodList){
-			foodName = String.format("%s", F.getClass());
-			if (foodName.contains(preyType)){
-				
-				distance = food.distance(this, F);
-				if (distance < closest && distance <= r){
-					closest = distance;
-					prey = F;
-				}
-			}
-		}
 	}
 }
-
-class grass extends food{
-
-	
-
-	grass(){
-		super();
-		foodType = "grass";
-	}
-	
-	grass(int x, int y){
-		super(x, y);
-		this.x = x;
-		this.y = y;
-		foodType = "grass";
-	}
-}
-
-
-
-class herbovore extends critter{
-	
-	
-	
-	herbovore(){
-		super();
-		prey = null;
-		speed = 5;
-		energy = 10;
-		preyType = "grass";
-		foodType = "herbovore";
-	}
-	
-	herbovore(int x, int y){
-		super(x, y);
-		prey = null;
-		speed = 5;
-		energy = 10;
-		preyType = "grass";
-		foodType = "herbovore";
-	}
-	
-}
-
-
